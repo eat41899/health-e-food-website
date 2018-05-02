ea7d46ae31f8d0087bb8898811d8484eaa2349f3
Eric Tapia <tapia.ea85@gmail.com>
2018-05-01 22:11:08 -0700

    Added scrolling to elements in scrip.js file and anchors in index file

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,11 +1,4 @@
 /*
- * Auto-generated content from the Brackets New Project extension.
- */
-
-/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
-/*global $, window, document */
-
-/*
 var waypoints = $('#handler-first').waypoint(function(direction) {
   notify(this.element.id + ' hit 25% from top of window') 
 }, {
@@ -16,6 +9,8 @@ var waypoints = $('#handler-first').waypoint(function(direction) {
 // Simple jQuery event handler
 $(document).ready(function () {
     "use strict";
+	
+	/*Sticky Navigation*/
     $('.js-section-features').waypoint(function(direction){
 		if (direction == "down"){
 			$('nav').addClass('sticky')
@@ -25,4 +20,33 @@ $(document).ready(function () {
 	}, {
   		offset: '60px;'
 	});
+	
+	/*Scrolling on buttons*/
+	$('.js-scroll-to-plan').click(function(){
+		$('html, body').animate({scrollTop: $('.js-section-plans').offset().top}, 1000);
+	});
+	
+	$('.js-scroll-to-start').click(function(){
+		$('html, body').animate({scrollTop: $('.js-section-features').offset().top}, 1000);
+	});
+	
+	/*Navigation-bar scrolling
+	* https://css-tricks.com/snippets/jquery/smooth-scrolling/
+	*/
+	$('a[href*=#]:not([href=#])').click(function() {
+    	if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+       	|| location.hostname == this.hostname) {
+
+        	var target = $(this.hash);
+        	target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           		if (target.length) {
+             		$('html,body').animate({
+                 		scrollTop: target.offset().top
+            			}, 1000);
+            		
+					return false;
+        		}
+    	}
+});
+	
 });
