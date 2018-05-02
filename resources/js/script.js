5d1dc841334330064eb644dd4bb7dd3383d285ce
Eric Tapia <tapia.ea85@gmail.com>
2018-05-02 16:57:34 -0700

    Added animation on scroll in sript file, added animation section to style.css

--- resources/js/script.js
+++ resources/js/script.js
@@ -30,8 +30,8 @@ $(document).ready(function () {
 		$('html, body').animate({scrollTop: $('.js-section-features').offset().top}, 1000);
 	});
 	
-	/*Navigation-bar scrolling
-	* https://css-tricks.com/snippets/jquery/smooth-scrolling/
+	/* Navigation-bar scrolling
+	*  https://css-tricks.com/snippets/jquery/smooth-scrolling/
 	*/
 	$('a[href*=#]:not([href=#])').click(function() {
     	if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
@@ -47,6 +47,35 @@ $(document).ready(function () {
 					return false;
         		}
     	}
-});
+	});
+	
+	/* Animations on scroll
+	* https://daneden.github.io/animate.css/
+	*/
+	$('.js-waypoint-1').waypoint(function(direction){
+		$('.js-waypoint-1').addClass('animated fadeIn');
+	}, {
+		offset: '50%'
+	});
+	$('.js-waypoint-2').waypoint(function(direction){
+		$('.js-waypoint-2').addClass('animated fadeInUp');
+	}, {
+		offset: '50%'
+	});
+	$('.js-waypoint-3').waypoint(function(direction){
+		$('.js-waypoint-3').addClass('animated fadeIn');
+	}, {
+		offset: '50%'
+	});
+	$('.js-waypoint-4').waypoint(function(direction){
+		$('.js-waypoint-4').addClass('animated pulse');
+	}, {
+		offset: '50%'
+	});
+	
+	
+	
+	
+	
 	
 });
